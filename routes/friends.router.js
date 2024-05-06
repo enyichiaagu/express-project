a82eea4b33984f4c89ba86152cf871756859a01e
enyichiaagu <enyichiaagu@gmail.com>
2024-05-06 09:24:18 +0100

    final files

--- routes/friends.router.js
+++ routes/friends.router.js
@@ -4,6 +4,11 @@ import friendsController from '../controllers/friends.controller.js';
 
 const friendsRouter = express.Router();
 
+friendsRouter.use((req, res, next) => {
+	console.log('IP Address', req.ip);
+	next();
+});
+
 friendsRouter.post('/', friendsController.postFriend);
 friendsRouter.get('/', friendsController.getFriends);
 friendsRouter.get('/:friendId', friendsController.getFriend);
