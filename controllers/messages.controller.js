5ad4e8cc531851c8b1f5733eafb22df3a80a1d7b
enyichiaagu <enyichiaagu@gmail.com>
2024-05-07 07:12:49 +0100

    added templating with ejs

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,12 +1,11 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import globalPath from '../utils/globalPath.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { __dirname } = globalPath(import.meta.url);
 
 function getMessages(req, res) {
-	res.sendFile(path.join(__dirname, '..', 'public', 'download.jpg'));
-	// res.send('<ul><li>Hello Albert</li></ul>');
+	res.sendFile(
+		path.join(__dirname, '..', 'public', 'images', 'download.jpg')
+	);
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+import path from 'node:path';
+
 import express from 'express';
 
 import friendsRouter from './routes/friends.router.js';
 import messagesRouter from './routes/messages.router.js';
 
+import globalPath from './utils/globalPath.js';
+
+const { __dirname } = globalPath(import.meta.url);
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
@@ -14,9 +23,15 @@ app.use((req, res, next) => {
 	console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
-// app.use(express.static('/public'));
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'My friends are very clever',
+		caption: 'Cool Cat',
+	});
+});
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 
